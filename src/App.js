5b7a154292dafc411dd6598f41f748e974969a68
kevinalbert7 <20ko@live.fr>
2021-10-25 14:24:52 +0200

    elsy

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './App.css';
+
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepsMin = 0
+const stepsMax = 50000
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="container-fluid">
+        <div class="row">
+          <p></p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
