0b41772f0cf3ac461043bbd66af5d8a540ef4596
kevinalbert7 <20ko@live.fr>
2022-10-10 00:56:14 +0200

    Exercice finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
-import Box from "./components/Box";
-import "./styles/global.css";
+import React from "react"
+
+import Step from "./components/Box"
+import "./styles/global.css"
 
 const tempMin = -20
 const tempMax = 40
@@ -10,22 +11,42 @@ const stepsMin = 0
 const stepsMax = 50000
 
 class App extends React.Component {
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          {/*Water*/}
-          <Box icon="local_drink" color="#3A85FF" value="1.5" unit="L"/>
-          {/*Steps*/}
-          <Box icon="directions_walk" color="black" value="3000" unit="steps"/>
-          {/*Heart*/}
-          <Box icon="favorite" color="red" value="120" unit="bpm"/>
-          {/*Temperature*/}
-          <Box icon="wb_sunny" color="yellow" value="-10" unit="°C"/>
+          <p>Heart : {heartMin}</p>
+          <p>Temperature : {tempMin}</p>
+          <p>Steps : {stepsMin}</p>
+          <Step 
+            icon="local_drink"
+            color="#3A85FF"
+            value={1.5}
+            unit="L"
+          />
+          <Step 
+            icon="directions_walk"
+            color="black"
+            value={3000}
+            unit="steps"
+          />
+          <Step 
+            icon="favorite"
+            color="red"
+            value={120}
+            unit="bpm"
+          />
+          <Step
+            icon="wb_sunny"
+            color="yellow"
+            value={-10}
+            unit="°C"
+          />
         </div>
       </div>
     )
-  };
+  }
 }
 
-export default App;
+export default App
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React, { Component } from 'react'
 
-
-class Box extends React.Component {
+class Step extends Component {
     render() {
         return (
-            <div className="box col-sm-3 col-6">
-                <span className={this.props.icon} {{fontSize: 100, this.props.box}}></span>
+            <div className='box  col-sm-3 col-6'>
+                <span className="material-icons" style={{fontSize: "100px", color: `${this.props.color}`, background: "transparent"}}>{this.props.icon}</span>
                 <p>{this.props.value}{this.props.unit}</p>
             </div>
         )
-    };
+    }
 }
 
-export default Box;
+export default Step
